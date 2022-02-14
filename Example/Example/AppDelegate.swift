41b144e6b5c92a491b267785e884ea76d4415880
saifullah ilyas <saifullah.ilyas@safarifoneict.com>
2022-02-14 23:33:23 +0500

    Example updated

--- Example/Example/AppDelegate.swift
+++ Example/Example/AppDelegate.swift
@@ -6,30 +6,38 @@
 //
 
 import UIKit
+import SFSideMenu
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
+var window: UIWindow?
+    var _drawerViewController : SFSideMenuViewController!
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        startApp()
+        
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    func startApp() {
+        let frame = UIScreen.main.bounds
+        window = UIWindow(frame: frame )
+        _drawerViewController = SFSideMenuViewController()
+        let vc = ViewController.instance()
+      //  let navigationController = UINavigationController(rootViewController: )
+        _drawerViewController?.centerViewController = vc
+        _drawerViewController?.leftViewController = LeftMenuview(frame: frame)
+       // _drawerViewController?.rightViewController = ViewController()
+        
+        window?.rootViewController = _drawerViewController
+        window?.makeKeyAndVisible()
+        //let menu =
+        //navigationController.viewControllers.append(menu)
+        //let vc = HomeServicesListingViewController.instantiate(from: StoryBoardName.home)
+       // navigationController.viewControllers.append(vc)
+        
     }
 
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
 
 
 }
--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -6,14 +6,81 @@
 //
 
 import UIKit
+import SFSideMenu
 
 class ViewController: UIViewController {
-
+    
+    
+   class func  instance()->Self {
+   return UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "ViewController") as! Self
+    }
+    var drawer : SFSideMenuViewController = SFSideMenuViewController()
     override func viewDidLoad() {
         super.viewDidLoad()
+       
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func toggle(_ sender: Any) {
+        let _drawer = (UIApplication.shared.delegate as! AppDelegate)._drawerViewController
+        _drawer?.openDrawer(.left, animated: true, complete: { _ in
+            
+        })
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+    }
 }
 
+class LeftMenuview : UIViewController {
+    var frame : CGRect!
+    init(frame : CGRect) {
+        super.init(nibName: nil, bundle: nil)
+        self.frame = frame
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    override func loadView() {
+        let rightView = UIView(frame: frame)
+        rightView.backgroundColor = .black
+        self.view = rightView
+    }
+}
+//class RightVC : UIViewController {
+//    var frame : CGRect!
+//    init(frame : CGRect) {
+//        super.init(nibName: nil, bundle: nil)
+//        self.frame = frame
+//    }
+//
+//    required init?(coder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
+//    override func loadView() {
+//        let rightView = UIView(frame: frame)
+//        rightView.backgroundColor = .black
+//        self.view = rightView
+//    }
+//}
+class ScaledHeightImageView: UIImageView {
+
+    override var intrinsicContentSize: CGSize {
+
+        if let myImage = self.image {
+            let myImageWidth = myImage.size.width
+            let myImageHeight = myImage.size.height
+            let myViewWidth = self.frame.size.width
+ 
+            let ratio = myViewWidth/myImageWidth
+            let scaledHeight = myImageHeight * ratio
+
+            return CGSize(width: myViewWidth, height: scaledHeight)
+        }
+
+        return CGSize(width: -1.0, height: -1.0)
+    }
+
+}
